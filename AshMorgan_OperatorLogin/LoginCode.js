26f2f3dc5600b21bee7ff9a3c73f6a743d460174
AshtenMorgan <ashmorga@uat.edu>
2022-06-03 03:14:38 -0500

    Bug Fixes and Added Comments
    Added Comments to html and .js
    
    Removed unnecessary code

--- AshMorgan_OperatorLogin/LoginCode.js
+++ AshMorgan_OperatorLogin/LoginCode.js
@@ -1,35 +1,33 @@
-function credCheck()                             
+function credCheck()   //Function for checking credentials of user input                          
 {
-    var firstName = document.forms[0].elements["FirstName"];              
-    var lastName = document.forms[0].elements["LastName"];   
-    var badge = document.forms[0].elements["Badge"];
+    var firstName = document.forms[0].elements["FirstName"];  //creates variable for input of First Name            
+    var lastName = document.forms[0].elements["LastName"];   //creates variable for input of Last Name
+    var badge = document.forms[0].elements["Badge"];   //creates variable for input of Badge Number
    
-    if (firstName.value == "")                                 
+    if (firstName.value == "")    //If nothing is input in the First Name input field                             
     {
-        window.alert("Please enter your first name.");
-        firstName.focus();
-        return false;
+        window.alert("Please enter your first name.");  
+        firstName.focus();  //highlights First Name input field
     }
     else if (lastName.value == "")                              
     {
         window.alert("Please enter your last name.");
-        lastName.focus();
-        return false;
+        lastName.focus();  //highlights Last Name input field
     }
-    else if (firstName.value.length + lastName.value.length > 21)                              
+    else if (firstName.value.length + lastName.value.length > 20)  //If the length of the input first and last name is greater than 20                            
     {
         window.alert("First and last name cannot total more than 20 characters.");
+        //highlights both first and last name input fields
+        firstName.focus();
         lastName.focus();
-        return false;
     }
     else if (badge.value.length != 3)                                  
     {
         window.alert("Please enter a 3-digit badge number.");
-        badge.focus();
-        return false;
+        badge.focus();  //highlights badge input field
     }
-    else
+    else //if no previous conditions are met (in this case, if all inputs are valid)
     {
-        window.alert("Logged In Successfully! Welcome " + firstName.value + " " + lastName.value + "!");
+        window.alert("Logged In Successfully! Welcome " + firstName.value + " " + lastName.value + "!"); //welcomes user with custom message
     }
 }
