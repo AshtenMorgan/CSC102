c2bea11e1709a47e61c533b857474a1e6215fdd4
AshtenMorgan <ashmorga@uat.edu>
2022-06-06 20:10:09 -0500

    Updated Sounds
    Updated Sound

--- AshMorgan_EventDrivenProgramming/EventDrivenCode.js
+++ AshMorgan_EventDrivenProgramming/EventDrivenCode.js
@@ -1,6 +1,6 @@
 function start()
 {
-    new Audio("StartSound.mp3").play()
+    new Audio("StartSound.mp3").play() //plays StartSound.mp3
     document.getElementById("startButton").disabled = true //disables start button when START is clicked
     document.getElementById("stopButton").disabled = false //enables stop button when START is clicked
 
@@ -8,7 +8,7 @@ function start()
 
 function stop()
 {
-    new Audio("StopSound.mp3").play()
+    new Audio("StopSound.mp3").play() //plays StopSound.mp3
     document.getElementById("stopButton").disabled = true //disables stop button when STOP is clicked
     document.getElementById("startButton").disabled = false //enables start button when STOP is clicked
 }
